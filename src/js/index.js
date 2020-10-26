24f8232a9e205b500ce87b8e610f95878aa90c0e
menaaziz27 <menaaziz27@gmail.com>
2020-10-26 23:43:21 +0200

    loader svg

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,30 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, loader, removeLoader } from './views/base';
 
 const state = {};
 
 const controlSearch = async () => {
 
+    // 1- get query from view
     const query = searchView.getInput();
 
     if (query) {
 
+        // 2- new search object and add to state
         state.search = new Search(query);
 
+        // 3- prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        loader(elements.searchRes);
+
+
+        // 4- search for recipe
         await state.search.getResults();
 
-        // console.log(state.search.result);
+        // 5- Render result on UI 
+        removeLoader();
         searchView.renderResults(state.search.result);
     }
 }
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,28 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
+    searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list')
+};
+
+export const elementStrings = {
+    loader: 'loader'
+}
+export const loader = parent => {
+
+    const loader = `
+    <div class=${elementStrings.loader}>
+        <svg>
+            <use href="img/icons.svg#icon-cw"></use>
+        </svg>
+    </div>
+    `;
+
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const removeLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    loader.remove();
+    // if (loader) loader.parentNode.removeChild(loader);
 }
