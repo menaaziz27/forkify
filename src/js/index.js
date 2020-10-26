b982a2962c3792c4e52b6af0dfd94bdb1be12686
menaaziz27 <menaaziz27@gmail.com>
2020-10-26 18:37:34 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
+const state = {};
+
+const controlSearch = async () => {
+
+    const query = searchView.getInput();
+
+    if (query) {
+
+        state.search = new Search(query);
+
+        await state.search.getResults();
+
+        // console.log(state.search.result);
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
