d988019cdbfcb4d5822118bf4f1777b0bce68d5a
menaaziz27 <menaaziz27@gmail.com>
2020-10-30 16:00:30 +0200

    list Controller

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,11 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, loader, removeLoader } from './views/base';
 
 const state = {};
+window.state = state;
 
 const controlSearch = async () => {
 
@@ -96,6 +98,34 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+const controlList = () => {
+    // Create a new list if there is none yet
+    if (!state.list) state.list = new List();
+
+    // add each ingredient to the list and UI 
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    })
+
+}
+
+// handle update and delete list item events 
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    // handle the delete button 
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // Delete from state
+        state.list.deleteItem(id);
+        // Delete from UI
+        listView.deleteItem(id);
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+})
+
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('btn-decrease, .btn-decrease *')) {
         // Decrease button is clicked 
@@ -107,6 +137,8 @@ elements.recipe.addEventListener('click', e => {
         // Increase button is clicked 
         state.recipe.updateServings('inc');
         recipeView.updateRecipeServings(state.recipe)
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controlList();
     }
 
 });
